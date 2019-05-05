006522afacb0b2ca2492401680c57de158599189
Andrii Maryskevych <andrii.maryskevych@gmail.com>
2019-05-05 09:12:49 +0300

    Add params DTO

---
+++ NMOMP2.0/DTO/Parameters.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NMOMP2._0.DTO
+{
+    class Parameters
+    {
+        public Parameters() {}
+
+        public int sizeX { get; set; }
+        public int sizeY { get; set; }
+        public int sizeZ { get; set; }
+
+        public int xAxisFEMCount { get; set; }
+        public int yAxisFEMCount { get; set; }
+        public int zAxisFEMCount { get; set; }
+
+        public double puasson { get; set; }
+        public double jung { get; set; }
+        public double pressure { get; set; }
+    }
+}
