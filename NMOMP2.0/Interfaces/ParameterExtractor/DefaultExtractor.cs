b3a6e5115a64535fe2c68a2a5204d8799145c4d0
Andrii Maryskevych <andrii.maryskevych@gmail.com>
2019-05-05 09:34:23 +0300

    Add interface and its simple implementation for params extraction

---
+++ NMOMP2.0/Interfaces/ParameterExtractor/DefaultExtractor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NMOMP2._0.DTO;
+
+namespace NMOMP2._0.Interfaces.ParameterExtractor
+{
+    class DefaultExtractor : IParameterExtractor
+    {
+        public DefaultExtractor() { }
+
+        public Parameters extract() {
+            Parameters parameters = new Parameters();
+
+            parameters.sizeX = 100;
+            parameters.sizeY = 100;
+            parameters.sizeZ = 100;
+
+            parameters.xAxisFEMCount = 2;
+            parameters.yAxisFEMCount = 2;
+            parameters.zAxisFEMCount = 2;
+
+            parameters.puasson = 0.3;
+            parameters.jung = 1;
+            parameters.pressure = -0.3;
+
+            return parameters;
+        }
+    }
+}
---
+++ NMOMP2.0/Interfaces/ParameterExtractor/IParameterExtractor.cs
@@ -0,0 +1,9 @@
+﻿using NMOMP2._0.DTO;
+
+namespace NMOMP2._0.Interfaces.ParameterExtractor
+{
+    interface IParameterExtractor
+    {
+        Parameters extract();
+    }
+}
